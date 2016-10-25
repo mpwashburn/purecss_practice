85b92097223a2eec263353c49ed85242849ac690
Michael Washburn <washburn.michael@gmail.com>
2016-10-24 20:49:59 -0700

    working menu bar that toggles

--- index.php
+++ index.php
@@ -10,19 +10,59 @@
   <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/pure_0.6.0/pure-min.css">
   <!-- <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css"> -->
   <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/pure_0.6.0/grids-responsive-min.css">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
   <!-- <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css"> -->
 </head>
 <body>
 <!-- Horizontal Menu/Navigation -->
-  <header class="custom-wrapper pure-g" id="menu">
+  <div class="custom-wrapper pure-g" id="menu">
+    <div class="pure-u-1 pure-u-md-1-3">
+        <div class="pure-menu">
+            <a href="#" class="pure-menu-heading custom-brand"><img class=" text-left img-responsive" src="http://placehold.it/50x50"></a>
+            <a href="#" class="custom-toggle" id="toggle"><s class="bar"></s><s class="bar"></s></a>
+        </div>
+    </div>
+    <div class="pure-u-1 pure-u-md-1-3">
+        <div class="pure-menu pure-menu-horizontal custom-menu-2 custom-can-transform">
+            <ul class="pure-menu-list">
+                <li class="pure-menu-item"><a href="https://www.facebook.com/ysnglobal/" onclick="target='newwindow'"><i class="fa fa-fw fa-facebook"></i></a></li>
+                <li class="pure-menu-item"><a href="https://twitter.com/ysn_global" onclick="target='newwindow'"><i class="fa fa-fw fa-twitter"></i></a></li>
+                <li class="pure-menu-item"><a href="http://instagram.com/ysnglobal/" onclick="target='newwindow'"><i class="fa fa-fw fa-instagram"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="pure-u-1 pure-u-md-1-3">
+        <div class="pure-menu pure-menu-horizontal custom-menu-3 custom-can-transform">
+            <ul class="pure-menu-list">
+                <li class="pure-menu-item"><a href="#" class="pure-menu-link">Home</a></li>
+                <li class="pure-menu-item"><a href="#" class="pure-menu-link">Company</a></li>
+                <li class="pure-menu-item"><a href="#" class="pure-menu-link">Team</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
 
+
+
+
+
+  <!-- <div class="custom-wrapper pure-g" id="menu">
     <div class="pure-u-1 pure-u-md-1-3">
       <div class="pure-menu">
         <a href="#" class="pure-menu-heading custom-brand">Brand</a>
         <a href="" class="custom-toggle" id="toggle"><s class="bar"></s>s class="bar"></s></a>
       </div>
     </div>
-  </header>
+    <div class="pure-u-1 pure-md-1-3">
+      <div class="pure-menu pure-menu-horizontal custom-can-transform">
+        <ul class="pure-menu-list">
+          <li class="pure-menu-item"><a href="#" class="pure-menu-link">Home</a></li>
+          <li class="pure-menu-item"><a href="#" class="pure-menu-link">Company</a></li>
+          <li class="pure-menu-item"><a href="#" class="pure-menu-link">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </div> -->
 
 
 <div class="pure-g">
